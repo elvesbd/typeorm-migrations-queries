3c3ccb6f47e720f56a3f054c70ac619caeba753e
Elves Brito <elvesbd@yahoo.com>
2021-08-21 11:57:55 -0300

    chore: configuring and created user migration

---
+++ src/db/migrations/1629557400682-UserMigration.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class UserMigration1629557400682 implements MigrationInterface {
+  name = 'UserMigration1629557400682';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar NOT NULL, "something" varchar NOT NULL)`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "user"`);
+  }
+}
